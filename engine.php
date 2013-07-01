3d795f6e57bf2cb7b9d022a7492d6e27c3d1b49e
raphaelCavalcante <raphael.cavalcante100@gmail.com>
2013-07-01 16:08:57 -0400

    Funcoes de criaçao de perfil e roupas

---
+++ engine.php
@@ -0,0 +1,46 @@
+<?php
+	/*$tipo;
+	$tamanho;
+	$estilo;
+	$cor;*/
+	class roupa{
+		function __contruct(){
+			$this->build();
+		}
+		function build(){
+			$arrayTipo=array();
+			
+		}
+		function criaRoupa($tipo1,$tamanho1,$estilo1,$cor1){
+			$arrayTipo=array();
+			array_push($arrayTipo,$tipo1);
+			array_push($arrayTipo,$tamanho1);
+			array_push($arrayTipo,$estilo1);
+			array_push($arrayTipo,$cor1);
+			return $arrayTipo;
+		}
+	}
+	class Engine{
+		function __contruct(){
+			$this->engine();
+		}
+		function __destruct(){
+			$this->close();
+		}
+		function genPerfil(){
+			$perfil=$obj->cavalo(rand(0,3), rand(0,4), rand(0,3), rand(0,4));
+			return $perfil;
+		}
+		function testNums(){
+			$arrayroupas=array();
+			for($i=0;$i<100;$i++){
+				$obj=new roupa();
+				$roupa=$obj->criaRoupa(rand(0,3), rand(0,4), rand(0,3), rand(0,4));
+				
+				array_push($arrayroupas,$roupa);
+			}
+			
+		return $arrayroupas;
+		}
+	}
+?>
